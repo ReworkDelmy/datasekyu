1a98bb48e74119330d5860108d7be1ddd9c35b63
ReworkDelmy <rozjebanamatura2020@gmail.com>
2021-03-22 18:30:51 +0100

    initalia

--- js/app.js
+++ js/app.js
@@ -16,24 +16,26 @@ const load = () => {
         case 0:
           header.textContent = 'Bezpieczeństwo i ochrona baz danych';
           img.src = './img/webDesign.png';
+          img.style.display = 'inline';
           main.style.background = '';
-          header.style.padding = '0px';
+
           header.style.background = 'transparent';
           break;
         case 1:
           header.textContent = 'Bezpieczne bazy danych';
-          img.src = '';
+          img.style.display = 'none';
           main.style.background = 'url(./img/security.jpg)';
           main.style.backgroundRepeat = 'no-repeat';
           main.style.backgroundSize = 'cover';
           header.style.background = '#000';
           header.style.color = '#fff';
-          header.style.padding = '20px';
+
           break;
         case 2:
           header.textContent = 'Przechowywanie danych';
+          img.style.display = 'inline';
           main.style.background = '';
-          header.style.padding = '0px';
+
           img.src = './img/dataStorage.png';
           header.style.background = 'transparent';
           break;
